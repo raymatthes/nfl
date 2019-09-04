870aab6914e696d9dae8649e47e5108b9dbdb4ac
Ray Matthes <ray.matthes@gmail.com>
2019-09-04 11:35:21 -0600

    2019 week 01

--- src/main/groovy/nfl/common/Constants.groovy
+++ src/main/groovy/nfl/common/Constants.groovy
@@ -14,6 +14,11 @@ class Constants {
       LAC, LAR, MIA, MIN, NE, NO, NYG, NYJ, OAK, PHI, PIT, SEA, SF, TB, TEN, WAS
    }
 
+   static enum Name_2018_2019 {
+      ARI, ATL, BAL, BUF, CAR, CHI, CIN, CLE, DAL, DEN, DET, GB, HOU, IND, JAX, KC,
+      LAC, LAR, MIA, MIN, NE, NO, NYG, NYJ, OAK, PHI, PIT, SEA, SF, TB, TEN, WAS
+   }
+
    static enum Name_2017_2018 {
       ARI, ATL, BAL, BUF, CAR, CHI, CIN, CLE, DAL, DEN, DET, GB, HOU, IND, JAX, KC,
       LAC, LAR, MIA, MIN, NE, NO, NYG, NYJ, OAK, PHI, PIT, SEA, SF, TB, TEN, WAS
